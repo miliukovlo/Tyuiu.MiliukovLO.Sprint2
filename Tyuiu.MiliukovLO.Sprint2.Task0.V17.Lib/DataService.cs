b74f1713309bb0c91db888fdbc78fedd06349395
ssadown <bibatututu@yandex.ru>
2024-09-23 18:37:06 +0500

    Task0

--- Tyuiu.MiliukovLO.Sprint2.Task0.V17.Lib/DataService.cs
+++ Tyuiu.MiliukovLO.Sprint2.Task0.V17.Lib/DataService.cs
@@ -6,8 +6,10 @@ namespace Tyuiu.MiliukovLO.Sprint2.Task0.V17.Lib
     {
         public bool[] GetCompareOperations(int x, int y)
         {
+
             bool[] result = [x == y, x != y, x < y, y > x, x <= y, y >= x];
             return result;
+        
         }
     }
 }
